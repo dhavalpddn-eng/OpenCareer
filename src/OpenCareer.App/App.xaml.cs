2877a7ae555110bc606bbd8b588b5f8afc77a6c9
dhavalpddn-eng <dhavalpddn@gmail.com>
2026-09-21 14:02:34 -0500

    feat: compose economy ledger persistence

--- src/OpenCareer.App/App.xaml.cs
+++ src/OpenCareer.App/App.xaml.cs
@@ -60,6 +60,7 @@ public partial class App : Microsoft.UI.Xaml.Application
         services.AddSingleton<IJobBoardStateStore, SqliteJobBoardStateStore>();
         services.AddSingleton<JobBoardGenerationService>();
         services.AddSingleton<IJobContractStore, SqliteJobContractStore>();
+        services.AddSingleton<IEconomyLedgerStore, SqliteEconomyLedgerStore>();
         services.AddSingleton<SqliteLogbookStore>();
         services.AddSingleton<ILogbookSource>(provider =>
             provider.GetRequiredService<SqliteLogbookStore>());
