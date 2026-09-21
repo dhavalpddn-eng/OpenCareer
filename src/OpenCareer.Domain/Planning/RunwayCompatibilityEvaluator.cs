c65a9831af9ab54343b5522ac0352b578a55d8a1
dhavalpddn-eng <dhavalpddn@gmail.com>
2026-09-20 21:47:34 -0500

    Add dispatch not-installed reason

--- src/OpenCareer.Domain/Planning/RunwayCompatibilityEvaluator.cs
+++ src/OpenCareer.Domain/Planning/RunwayCompatibilityEvaluator.cs
@@ -19,6 +19,7 @@ public enum DispatchEndpoint
 public enum DispatchFeasibilityReason
 {
     AircraftNotFound = 0,
+    AircraftNotInstalled,
     AircraftCapabilityDataIncomplete,
     AircraftRunwayPerformanceUnknown,
     AirportNotFound,
