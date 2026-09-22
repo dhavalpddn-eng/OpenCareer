50c08897a00b3088a24bbd2cd2c4a9e4d41ed26b
dhavalpddn-eng <dhavalpddn@gmail.com>
2026-09-22 11:19:46 -0500

    feat: derive persisted flight cost basis

--- src/OpenCareer.App/App.xaml.cs
+++ src/OpenCareer.App/App.xaml.cs
@@ -191,6 +191,9 @@ public partial class App : Microsoft.UI.Xaml.Application
         services.AddSingleton<StandardPointToPointMissionCompletionSource>();
         services.AddSingleton<ICareerJobMissionCompletionSource>(provider =>
             provider.GetRequiredService<StandardPointToPointMissionCompletionSource>());
+        services.AddSingleton<PersistedFlightSettlementCostsSource>();
+        services.AddSingleton<ICareerJobSettlementCostsSource>(provider =>
+            provider.GetRequiredService<PersistedFlightSettlementCostsSource>());
         services.AddSingleton<CareerJobCompletionInputSource>();
 
         services.AddSingleton<ITutorialCatalog, AppTutorialCatalog>();
